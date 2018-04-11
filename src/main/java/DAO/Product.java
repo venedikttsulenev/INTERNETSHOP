e8722b979d293c7c7c1de950863af6363f6f39b7
Sergey <gray4747@mail.ru>
2018-04-11 19:11:46 +0300

    DB start

---
+++ src/main/java/DAO/Product.java
@@ -0,0 +1,54 @@
+package DAO;
+
+public class Product {
+    private int id;
+    private String name;
+    private String description;
+    private int count;
+    private int price;
+
+    public Product() {
+
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
