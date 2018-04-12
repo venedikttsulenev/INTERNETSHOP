2d1ac00fd04ea5ff042a0ac3250738af83efb510
Matvei <mattyou777@gmail.com>
2018-04-12 20:05:28 +0300

    Added package commands with LoginCommand and NoCommand, package manager with ConfigurationManager and MassageManager, RequestHelper, LoginLogic service config and messages properties, error, login and main jsps

---
+++ src/main/java/controllers/commands/Command.java
@@ -0,0 +1,14 @@
+package controllers.commands;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+public interface Command {
+
+    public String execute(HttpServletRequest request,
+                          HttpServletResponse response)
+            throws ServletException, IOException;
+
+}
