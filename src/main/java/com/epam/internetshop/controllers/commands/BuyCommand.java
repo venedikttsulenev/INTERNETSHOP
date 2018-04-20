cfc3c174fec5327cffbc6c0c2c89426013c1e304
venedikttsulenev <venedikttsulenev@list.ru>
2018-04-20 13:27:54 +0300

    Created bucket page core. Created BuyCommand and RemoveFromCartCommand. Changed main controller url to '/process'.

---
+++ src/main/java/com/epam/internetshop/controllers/commands/BuyCommand.java
@@ -0,0 +1,19 @@
+package com.epam.internetshop.controllers.commands;
+
+import com.epam.internetshop.controllers.manager.ConfigurationManager;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+
+public class BuyCommand implements Command {
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response) {
+        HttpSession session = request.getSession();
+        HashMap<Long, Integer> bucketList = (HashMap<Long, Integer>) session.getAttribute("bucketList");
+        /* TODO: Perform payment */
+        bucketList.clear();
+        return ConfigurationManager.getInstance().getProperty(ConfigurationManager.MAIN_PAGE_PATH);
+    }
+}
