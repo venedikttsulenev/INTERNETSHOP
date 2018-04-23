66e3c4fd0794936768bb505204e12b2ba8b02ab4
venedikttsulenev <venedikttsulenev@list.ru>
2018-04-23 16:20:24 +0300

    Have impelmented command logic Fixed bug with outputting product count as a placeholder instead of value

--- src/main/java/com/epam/internetshop/controllers/logic/ChangeProductCountLogic.java
+++ src/main/java/com/epam/internetshop/controllers/logic/ChangeProductCountLogic.java
@@ -1,13 +1,13 @@
 package com.epam.internetshop.controllers.logic;
 
 import com.epam.internetshop.services.ProductService;
+import com.epam.internetshop.services.exception.ProductException;
 import com.epam.internetshop.services.manager.ServiceFactory;
 
 public class ChangeProductCountLogic {
     private static final ProductService productService = ServiceFactory.newProductService();
 
-    public static boolean changeProductCount(Long productId, Long newCount) {
-        /* TODO */
-        return false;
+    public static boolean changeProductCount(Long productId, Long newCount) throws ProductException {
+        return (null != productService.setCount(productId, newCount));
     }
 }
