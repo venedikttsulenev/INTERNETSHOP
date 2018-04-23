e154ba856c50422608b0d40c2f1e585263883f5f
Sergey <gray4747@mail.ru>
2018-04-23 14:24:11 +0300

    More tests added.

--- src/test/java/com/epam/internetshop/test/TestPayment.java
+++ src/test/java/com/epam/internetshop/test/TestPayment.java
@@ -6,12 +6,13 @@ import com.epam.internetshop.domain.User;
 import com.epam.internetshop.services.PaymentService;
 import com.epam.internetshop.services.ProductService;
 import com.epam.internetshop.services.UserService;
-import com.epam.internetshop.services.impl.PaymentServiceImpl;
+import com.epam.internetshop.services.exception.ProductException;
+import com.epam.internetshop.services.exception.UserException;
 import com.epam.internetshop.services.manager.ServiceFactory;
 import org.junit.Test;
-import org.junit.Assert.*;
 
 import java.util.Date;
+import java.util.HashMap;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
@@ -24,7 +25,7 @@ public class TestPayment {
     @Test
     public void create() {
         Product product = productService.create(new Product("testing", null, 10L, 10L));
-        User user = userService.createUser("TestPayment","12345678");
+        User user = userService.createUser("TestPayment", "12345678");
         assertNotEquals(null,
                 paymentService.create(new Payment(user, product, 10L, 10L, new Date())));
     }
@@ -40,8 +41,54 @@ public class TestPayment {
     @Test
     public void createWrongProduct() {
         Product product = null;
-        User user = userService.createUser("PaymentAdmin2","12345678");
+        User user = userService.createUser("PaymentAdmin2", "12345678");
         assertEquals(null,
                 paymentService.create(new Payment(user, product, 10L, 10L, new Date())));
+    }
+
+    @Test(expected = UserException.class)
+    public void testPaymentNotEnoughCash() {
+        User user = userService.create(new User("paymentTest", "12345678", 0L, false, false));
+        Product product = productService.create(
+                new Product("name", "disc", 10L, 10L));
+        HashMap<Product, Long> map = new HashMap<>();
+        map.put(product, 1L);
+        paymentService.performPayment(user.getLogin(), map);
+    }
+
+    @Test
+    public void testPayment() {
+        User user = userService.create(new User("paymentTest1", "12345678", 10L, false, false));
+        Product product = productService.create(
+                new Product("name", "disc", 10L, 10L));
+        HashMap<Product, Long> map = new HashMap<>();
+        map.put(product, 1L);
+        paymentService.performPayment(user.getLogin(), map);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testPaymentNullLogin() {
+        Product product = productService.create(
+                new Product("name", "disc", 10L, 10L));
+        HashMap<Product, Long> map = new HashMap<>();
+        map.put(product, 1L);
+        paymentService.performPayment(null, map);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testPaymentNullMap() {
+        User user = userService.create(new User("paymentTest2", "12345678", 10L, false, false));
+        paymentService.performPayment(user.getLogin(), null);
+    }
+
+    @Test(expected = ProductException.class)
+    public void testPaymentNotEnoughProduct() {
+        User user = userService.create(
+                new User("paymentTest3", "12345678", 10L, false, false));
+        Product product = productService.create(
+                new Product("name", "disc", 1L, 10L));
+        HashMap<Product, Long> map = new HashMap<>();
+        map.put(product, 2L);
+        paymentService.performPayment(user.getLogin(), map);
     }
 }
